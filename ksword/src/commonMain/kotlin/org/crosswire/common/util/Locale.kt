3cdc55c6faf5a8e1aff5f87f38be7e23b4596b8d
mjdenham <mjdenham@gmail.com>
2025-10-04 16:24:20 +0100

    Set app locale

--- ksword/src/commonMain/kotlin/org/crosswire/common/util/Locale.kt
+++ ksword/src/commonMain/kotlin/org/crosswire/common/util/Locale.kt
@@ -11,8 +11,23 @@ enum class Locale(val languageCode: String) {
     companion object {
         var current: Locale = ENGLISH
 
+        /**
+         * Find a Locale enum by its language code.
+         * @param languageCode ISO 639-1 language code (e.g., "en", "es")
+         * @return matching Locale or ENGLISH as fallback
+         */
         fun findLocale(languageCode: String): Locale {
             return entries.find { it.languageCode == languageCode } ?: ENGLISH
+        }
+
+        /**
+         * Set the default locale from a language code.
+         * Automatically finds and sets the matching Locale enum.
+         *
+         * @param languageCode ISO 639-1 language code (e.g., "en", "es")
+         */
+        fun setDefaultLocale(languageCode: String) {
+            current = findLocale(languageCode)
         }
     }
 }
