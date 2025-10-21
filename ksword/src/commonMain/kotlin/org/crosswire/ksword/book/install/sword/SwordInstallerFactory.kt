367142bcedff092c6bdd7ace1dcf93c69bae0464
mjdenham <mjdenham@gmail.com>
2025-10-21 20:32:13 +0100

    Support multiple repositories

--- ksword/src/commonMain/kotlin/org/crosswire/ksword/book/install/sword/SwordInstallerFactory.kt
+++ ksword/src/commonMain/kotlin/org/crosswire/ksword/book/install/sword/SwordInstallerFactory.kt
@@ -4,8 +4,19 @@ class SwordInstallerFactory {
     companion object {
         val CROSSWIRE_INSTALLER_URLS = InstallerUrls("CrossWire", "www.crosswire.org", "/ftpmirror/pub/sword/packages/rawzip", "/ftpmirror/pub/sword/raw")
         val EBIBLE_INSTALLER_URLS = InstallerUrls("eBible", "ebible.org", "/sword/zip", "/sword")
+        val LOCKMAN_INSTALLER_URLS = InstallerUrls("Lockman", "www.crosswire.org", "/ftpmirror/pub/sword/lockmanpackages", "/ftpmirror/pub/sword/lockmanraw")
     }
 
     val crosswireInstaller = HttpsSwordInstaller(CROSSWIRE_INSTALLER_URLS)
     val ebibleInstaller = HttpsSwordInstaller(EBIBLE_INSTALLER_URLS)
+    val lockmanInstaller = HttpsSwordInstaller(LOCKMAN_INSTALLER_URLS)
+
+    suspend fun findInstaller(initials: String): HttpsSwordInstaller {
+        listOf(crosswireInstaller, lockmanInstaller, ebibleInstaller).forEach { installer ->
+            if (installer.getBooks().find { it.initials == initials } != null) {
+                return installer
+            }
+        }
+        throw Exception("No installer found for initials: $initials")
+    }
 }
